4756ce723e1b4aaa18efe3a781212a0032c00fdf
ervin <ervin.cabascango.s@gmail.com>
2021-07-05 01:03:23 -0500

    style: view home

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MoviesPosterGridComponent } from './movies-poster-grid/movies-poster-grid.component';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [NavbarComponent, SlideshowComponent, MoviesPosterGridComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RatingModule],
   exports: [NavbarComponent, SlideshowComponent, MoviesPosterGridComponent],
 })
 export class ComponentsModule {}
